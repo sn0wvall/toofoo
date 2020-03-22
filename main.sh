bbf2212d54316025e0283ec08304320debbf82c1
sn0wvall <southernquack@protonmail.com>
2020-03-22 22:37:22 +0000

    Added deleteEvent function (incomplete)

--- main.sh
+++ main.sh
@@ -5,23 +5,39 @@
 calFormat="dd/mm/yy"
 calFileDest="/home/alpha/.toofoo/cal"
 purpose=$1
+IFS=$'\r\n' GLOBIGNORE='*' command eval 'calFile=($(cat $calFileDest))'
+x=0
 
+# Define Core Functions
 
 printEvents(){
-	IFS=$'\r\n' GLOBIGNORE='*' command eval 'calFile=($(cat $calFileDest))'
 	
 	for i in "${calFile[@]}"
 	do
 		echo "$i"
 	done
+
 }
 
 newEvent(){
+
 	eventDetailsCreate="$1"
 	eventDateCreate="$2"
 
 	echo "$eventDetailsCreate"   >> "$calFileDest"
 	echo "$eventDateCreate"      >> "$calFileDest"
+}
+
+deleteEvent(){
+
+	target="$1"
+	while [ $x -lt ${#calFile[@]} ]
+	do
+		test "${calFile[$x]}" = "$target" && sed "/$x/d" $calFileDest &>/dev/null && return 0
+		((x=x+1))
+	done
+	echo "Event Name not Found"
+
 }
 
 case $purpose in
@@ -30,5 +46,9 @@ case $purpose in
 	     	printf "Date:"; read eventDate
 
 	     	newEvent "$eventDetails" "$eventDate"				;;
+	
 	show) 	printEvents							;;
+
+	del)	printf "Event Name to Delete: "; read deleteTarget
+		deleteEvent "$deleteTarget"					;;
 esac
