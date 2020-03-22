44b4dfb7f825ff5090bcd2517fbf69ab5c6f9695
sn0wvall <southernquack@protonmail.com>
2020-03-22 22:04:39 +0000

    Added printEvents and newEvent function
    Technically, the program now has core functionality but you know, TECHNICALLY

--- main.sh
+++ main.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
+# Define Core Variables
 
+calFormat="dd/mm/yy"
+calFileDest="/home/alpha/.toofoo/cal"
+purpose=$1
+
+
+printEvents(){
+	IFS=$'\r\n' GLOBIGNORE='*' command eval 'calFile=($(cat $calFileDest))'
+	
+	for i in "${calFile[@]}"
+	do
+		echo "$i"
+	done
+}
+
+newEvent(){
+	eventDetailsCreate="$1"
+	eventDateCreate="$2"
+
+	echo "$eventDetailsCreate"   >> "$calFileDest"
+	echo "$eventDateCreate"      >> "$calFileDest"
+}
+
+case $purpose in
+
+	add) 	printf "New event:"; read eventDetails
+	     	printf "Date:"; read eventDate
+
+	     	newEvent "$eventDetails" "$eventDate"				;;
+	show) 	printEvents							;;
+esac
