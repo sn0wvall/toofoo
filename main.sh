91e1074355df1922c7730a19f6de9e780e84029c
sn0wvall <southernquack@protonmail.com>
2020-03-23 11:33:26 +0000

    Finished deleteEvent function
    Program properly works now! This is a start - calendar organisation is next

--- main.sh
+++ main.sh
@@ -7,15 +7,20 @@ calFileDest="/home/alpha/.toofoo/cal"
 purpose=$1
 IFS=$'\r\n' GLOBIGNORE='*' command eval 'calFile=($(cat $calFileDest))'
 x=0
+y=1
 
 # Define Core Functions
 
 printEvents(){
-	
-	for i in "${calFile[@]}"
-	do
-		echo "$i"
-	done
+
+	case $1 in
+		today)	echo "Showing events today"		;;
+		*)	echo "Showing all Events"; echo
+			for i in "${calFile[@]}"
+			do
+				echo "$i"
+			done					;;
+	esac
 
 }
 
@@ -24,7 +29,7 @@ newEvent(){
 	eventDetailsCreate="$1"
 	eventDateCreate="$2"
 
-	echo "$eventDetailsCreate"   >> "$calFileDest"
+	echo "$eventDetailsCreate"   >> "$calFileDest"				# In future, ordering the events in the file by date may be a useful feature.
 	echo "$eventDateCreate"      >> "$calFileDest"
 }
 
@@ -33,8 +38,9 @@ deleteEvent(){
 	target="$1"
 	while [ $x -lt ${#calFile[@]} ]
 	do
-		test "${calFile[$x]}" = "$target" && sed "/$x/d" $calFileDest &>/dev/null && return 0
-		((x=x+1))
+		test "${calFile[$x]}" = "$target" && sed -i "$y"d $calFileDest && sed -i "$y"d $calFileDest && printf "\nDeleted Event ${calFile[$x]}" && return 0
+		((x=x+2))			# Optimally, I would have used some inbuilt (()) style maths to calculate line number, however sed wouldn't work with it
+		((y=y+2))			# Therefore, I opted for a more ham-fisted solution of using two variables.
 	done
 	echo "Event Name not Found"
 
@@ -45,10 +51,11 @@ case $purpose in
 	add) 	printf "New event:"; read eventDetails
 	     	printf "Date:"; read eventDate
 
-	     	newEvent "$eventDetails" "$eventDate"				;;
+	     	newEvent "$eventDetails" "$eventDate"						;;
 	
-	show) 	printEvents							;;
+	show) 	printEvents $2									;;
 
 	del)	printf "Event Name to Delete: "; read deleteTarget
-		deleteEvent "$deleteTarget"					;;
+			deleteEvent "$deleteTarget"						;;
+	*)		echo "\"$1\" is not a known command"					;;
 esac
